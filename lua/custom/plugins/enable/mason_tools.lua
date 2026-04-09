fdd4f52c074f3740a5ddff1a7eba29984267c1e1
hlstwizard <h.l.s.t@163.com>
2026-04-09 16:30:26 +0800

    chore: disable neotree/opencode and refresh plugins

--- lua/custom/plugins/enable/mason_tools.lua
+++ lua/custom/plugins/enable/mason_tools.lua
@@ -4,6 +4,7 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       if not vim.tbl_contains(opts.ensure_installed, 'prettier') then table.insert(opts.ensure_installed, 'prettier') end
+      if not vim.tbl_contains(opts.ensure_installed, 'stylua') then table.insert(opts.ensure_installed, 'stylua') end
     end,
   },
 }
